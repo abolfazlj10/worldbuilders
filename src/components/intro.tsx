c8602d34dfbf4efae07ac45440adec08caf5c64c
abolfazl <abolfazljamshidi101010@gmail.com>
2025-08-29 15:59:26 +0330

    change layout main section to grid system

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,19 +1,50 @@
+// import Navigation from "./navigation";
+
+// const Intro = () => {
+//     return(
+//         <div className="relative h-dvh w-dvw font-pixelify flex items-center justify-center overflow-hidden">
+//             <div className="w-full -rotate-90 mt-20 -space-y-3">
+//                 <div className="text-5xl opacity-75">(Digital)</div>
+//                 <div className="text-[100px] flex gap-7 opacity-90">
+//                     <span>W</span>
+//                     <img src="/image/pxArt__1_-removebg-preview 1.png" alt="w" className="object-contain w-[100px]" />
+//                     <span>r</span>
+//                     <span>l</span>
+//                     <span>d</span>
+//                     <img src="/image/_.png" alt="w" className="object-contain w-[80px] ml-7" />
+//                 </div>
+//                 <div className="text-[100px] flex gap-7 opacity-90 -mt-14">
+//                     <span>B</span>
+//                     <span>u</span>
+//                     <span className="text-secondBlue font-IBM">!</span>
+//                     <span>l</span>
+//                     <span>d</span>
+//                     <span>3</span>
+//                     <span>r</span>
+//                 </div>
+//                 <div className="text-6xl opacity-75 flex justify-end -mt-10 -mr-40">zzz</div>
+//             </div>
+//             <Navigation />
+//         </div>
+//     )
+// }
+// export default Intro;
 import Navigation from "./navigation";
 
 const Intro = () => {
     return(
         <div className="relative h-dvh w-dvw font-pixelify flex items-center justify-center overflow-hidden">
-            <div className="w-full -rotate-90 mt-20 -space-y-3">
-                <div className="text-5xl opacity-75">(Digital)</div>
-                <div className="text-[100px] flex gap-7 opacity-90">
+            <div className="grid grid-cols-4 grid-rows-7 h-full pb-40 pr-3">
+                <div className="text-5xl opacity-75 text-vertical row-span-7 flex items-end">(Digital)</div>
+                <div className="text-[100px] flex items-center gap-7 opacity-90 text-vertical row-span-7">
                     <span>W</span>
                     <img src="/image/pxArt__1_-removebg-preview 1.png" alt="w" className="object-contain w-[100px]" />
                     <span>r</span>
                     <span>l</span>
                     <span>d</span>
-                    <img src="/image/_.png" alt="w" className="object-contain w-[80px] ml-7" />
+                    <img src="/image/_.png" alt="w" className="object-contain w-[80px]" />
                 </div>
-                <div className="text-[100px] flex gap-7 opacity-90 -mt-14">
+                <div className="text-[100px] flex gap-7 opacity-90 text-vertical row-span-7">
                     <span>B</span>
                     <span>u</span>
                     <span className="text-secondBlue font-IBM">!</span>
@@ -22,7 +53,7 @@ const Intro = () => {
                     <span>3</span>
                     <span>r</span>
                 </div>
-                <div className="text-6xl opacity-75 flex justify-end -mt-10 -mr-40">zzz</div>
+                <div className="text-6xl opacity-75 text-vertical row-span-3">zzz</div>
             </div>
             <Navigation />
         </div>
