e173d271aa57e63973c51b5aaac733d1818fca32
abolfazl <abolfazljamshidi101010@gmail.com>
2025-08-30 12:01:41 +0330

    add custom cursor and fix desktop intor

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,6 @@
 const Navigation = () => {
     return(
-        <div className="absolute w-full h-32 bottom-0 flex items-center justify-center text-3xl rotate-90">{`>`}</div>
+        <div className="absolute w-full h-32 bottom-0 flex items-center justify-center text-3xl rotate-90 md:rotate-0">{`>`}</div>
     )
 }
 export default Navigation;
