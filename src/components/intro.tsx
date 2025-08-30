4ed073834d52ffb86c131a6e76296c870eeae4ad
abolfazl <abolfazljamshidi101010@gmail.com>
2025-08-30 12:09:05 +0330

    change position digitals text

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -34,8 +34,8 @@ import Navigation from "./navigation";
 const Intro = () => {
     return(
         <div className="relative h-dvh w-dvw font-pixelify flex items-center justify-center overflow-hidden">
-            <div className="grid grid-cols-4 pb-40 mt-auto md:grid-cols-none md:w-full md:pb-0  md:mt-0">
-                <div className="text-5xl opacity-75 max-md:[writing-mode:vertical-rl] max-md:[transform:rotate(180deg)] md:[writing-mode:horizontal-tb] flex items-end">(Digital)</div>
+            <div className="grid grid-cols-4 pb-32 mt-auto md:grid-cols-none md:w-full md:pb-0  md:mt-0">
+                <div className="text-5xl opacity-75 max-md:[writing-mode:vertical-rl] max-md:[transform:rotate(180deg)] md:[writing-mode:horizontal-tb] flex items-center">(Digital)</div>
                 <div className="text-[100px] md:text-[120px] flex items-center md:justify-between justify-start gap-7 opacity-90 max-md:[writing-mode:vertical-rl] max-md:[transform:rotate(180deg)] md:[writing-mode:horizontal-tb] md:pr-40">
                     <span>W</span>
                     <a target="_blank" href="https://figma.com/design/8lVohgcnbYHG5rFeQAXWGL/-Worlds?t=mjVdpjeN7CaeZUiN-0"><img src="/image/pxArt__1_-removebg-preview 1.png" alt="w" className="object-contain w-[120px] hover:cursor-[url('/image/linkCursor.png'),_pointer]" /></a>
