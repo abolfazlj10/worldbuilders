f3ee7002630892a06c1a8a57f77005e32ea69e97
abolfazl <abolfazljamshidi101010@gmail.com>
2025-08-31 16:07:53 +0330

    resize elements and texts

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -4,7 +4,7 @@ const Intro = () => {
     return(
         <div id="intor" tabIndex={1} className="relative h-dvh w-dvw flex items-center justify-center overflow-hidden">
             <div className="grid grid-cols-4 pb-32 mt-auto md:grid-cols-none md:w-full md:pb-0  md:mt-0">
-                <div className="text-5xl opacity-65 max-md:[writing-mode:vertical-rl] max-md:[transform:rotate(180deg)] md:[writing-mode:horizontal-tb] flex items-center">(Digital)</div>
+                <div className="text-4xl opacity-65 max-md:[writing-mode:vertical-rl] max-md:[transform:rotate(180deg)] md:[writing-mode:horizontal-tb] flex items-center md:-mb-7">(Digital)</div>
                 <div className="text-[100px] md:text-[120px] flex items-center md:justify-between justify-start gap-7 opacity-80 max-md:[writing-mode:vertical-rl] max-md:[transform:rotate(180deg)] md:[writing-mode:horizontal-tb] md:pr-40">
                     <span>W</span>
                     <a target="_blank" href="https://figma.com/design/8lVohgcnbYHG5rFeQAXWGL/-Worlds?t=mjVdpjeN7CaeZUiN-0"><img src="/image/pxArt__1_-removebg-preview 1.png" alt="w" className="object-contain w-[120px] hover:cursor-[url('/image/linkCursor.png'),_pointer]" /></a>
@@ -13,16 +13,16 @@ const Intro = () => {
                     <span>D</span>
                     <img src="/image/_.png" alt="w" className="object-contain w-[100px]" />
                 </div>
-                <div className="text-[100px] md:text-[120px] flex items-center justify-start md:justify-between gap-7 opacity-80 max-md:[writing-mode:vertical-rl] max-md:[transform:rotate(180deg)] md:[writing-mode:horizontal-tb] relative">
+                <div className="text-[100px] md:text-[120px] flex items-center justify-start md:justify-between gap-7 opacity-80 max-md:[writing-mode:vertical-rl] max-md:[transform:rotate(180deg)] md:[writing-mode:horizontal-tb] relative md:-mt-11">
                     <span>B</span>
                     <span>U</span>
-                    <span className="text-secondBlue font-IBM">!</span>
+                    <span className="text-secondBlue font-IBM font-bold">!</span>
                     <span>L</span>
                     <span>D</span>
                     <span>3</span>
                     <span>R</span>
                 </div>
-                <div className="text-6xl opacity-65 max-md:[writing-mode:vertical-rl] max-md:[transform:rotate(180deg)] md:[writing-mode:horizontal-tb] mb-auto md:text-[100px] md:ml-auto">zzz</div>
+                <div className="text-6xl opacity-65 max-md:[writing-mode:vertical-rl] max-md:[transform:rotate(180deg)] md:[writing-mode:horizontal-tb] mb-auto md:text-[100px] md:ml-auto md:-mt-15">zzz</div>
             </div>
             <Navigation theHref="#AboutWe" />
         </div>
--- src/components/services.tsx
+++ src/components/services.tsx
@@ -2,26 +2,26 @@ import Navigation from "./navigation"
 
 const Services = () => {
     return(
-        <div id="services" tabIndex={3} className="h-dvh w-dvw relative">
+        <div id="services" tabIndex={3} className="h-dvh w-dvw relative overflow-x-hidden">
             <div className="px-5 md:px-0 h-full">
                 <div className="flex justify-between pt-20 px-10 md:px-24 md:pt-10 md:relative">
-                    <div className="text-secondBlue text-[55px] md:text-[70px] font-pixelify">SERVICESSSS</div>
-                    <img src="/image/_.png" alt="w" className="object-contain w-[100px] md:w-[170px] absolute bottom-10 -left-8 md:-top-5 md:-right-8 md:left-auto md:bottom-auto" />
+                    <div className="text-secondBlue text-[55px] md:text-[70px] font-pixelify font-bold">SERVICESSSS</div>
+                    <img src="/image/_.png" alt="w" className="object-contain w-[100px] md:w-[150px] absolute bottom-10 -left-8 md:-top-5 md:-right-8 md:left-auto md:bottom-auto" />
                 </div>
-                <div className="flex justify-between md:text-[100px] text-[70px] w-8/12 md:w-full mt-40 md:mt-0">
+                <div className="flex justify-between md:text-[80px] text-[70px] w-8/12 md:w-full mt-40 md:mt-0">
                     <span>W</span>
                     <span>o</span>
                     <span>r</span>
                     <span>l</span>
                     <span>d</span>
                 </div>
-                <div className="font-IBM md:text-center md:text-[32px] text-[20px] md:tracking-[16px] tracking-[1px] opacity-75 font-thin">WorldBuilderzzz Style & Unique</div>
+                <div className="font-IBM md:text-center md:text-[22px] text-[20px] md:tracking-[16px] tracking-[1px] opacity-75 ">WorldBuilderzzz Style & Unique</div>
                 <div className="flex justify-between md:text-[100px] text-[70px] md:w-6/12 w-4/12 mt-16 md:mt-0">
                     <span>W</span>
                     <span>e</span>
                     <span>b</span>
                 </div>
-                <div className="flex items-center justify-around font-IBM md:text-[32px] text-[20px] tracking-[3px] opacity-75">
+                <div className="flex items-center justify-around font-IBM md:text-[22px] text-[20px] tracking-[3px] opacity-75">
                     <div>Fast</div>
                     <div>*</div>
                     <div>Standard</div>
