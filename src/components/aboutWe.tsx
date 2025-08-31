f830958de81a508f3731c1aa95cbfdeff1273cd0
abolfazl <abolfazljamshidi101010@gmail.com>
2025-08-31 12:59:07 +0330

    finish about we

--- src/components/aboutWe.tsx
+++ src/components/aboutWe.tsx
@@ -2,15 +2,15 @@ import Navigation from "./navigation"
 const  AboutWe = () => {
     return(
         <div id="AboutWe" tabIndex={2} className="h-dvh w-dvw overflow-hidden relative">
-            <div className="flex flex-col py-20 px-10">
-                <div className="text-secondBlue text-[55px]">WHO ARE WE?</div>
-                <img src="/image/_.png" alt="w" className="object-contain w-[100px] ml-auto -mr-15 mt-14 mb-4" />
-                <div className="font-IBM text-[18px] leading-[40px] font-[400] flex-1 tracking-wide">
-                    <span className="opacity-85">We are not just a design studio. We are a team of</span> <span className="font-bold underline decoration-secondBlue decoration-[3px] uppercase">world builders</span> <span className="opacity-85">! creators who translate visions, vibes, and unique universes into tangible, digital experiences. Every project we take on is a canvas for a larger idea: turning a brand’s essence into a living, breathing</span> <span className="font-bold opacity-100">world</span>.
+            <div className="flex flex-col py-20 px-10 md:px-24 md:py-10">
+                <div className="text-secondBlue text-[55px] md:text-[70px]">WHO ARE WE?</div>
+                <img src="/image/_.png" alt="w" className="object-contain w-[100px] ml-auto -mr-15 mt-14 mb-4 md:order-3 md:absolute md:-bottom-10 md:w-[140px] md:left-10" />
+                <div className="font-IBM text-[18px] leading-[40px] font-[400] flex-1 tracking-wide md:tracking-[15px] md:text-[30px] md:leading-[50px] md:text-justify">
+                    <span className="opacity-70">We are not just a design studio. We are a team of</span> <span className="font-bold md:tracking-[20px] underline decoration-secondBlue decoration-[3px] uppercase">world builders</span> <span className="opacity-85">! creators who translate visions, vibes, and unique universes into tangible, digital experiences. Every project we take on is a canvas for a larger idea: turning a brand’s essence into a living, breathing</span> <span className="font-bold opacity-100">world</span>.
                 </div>
             </div>
             <Navigation theHref="#services" />
         </div>
     )
 }
-export default AboutWe
+export default AboutWe;
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,6 @@
 const Navigation = ({theHref} : {theHref: string}) => {
     return(
-        <div className="absolute w-full h-32 bottom-0 flex items-center justify-center"><a href={theHref} className="text-3xl rotate-90 md:rotate-0">{`>`}</a></div>
+        <div className="absolute w-full h-32 bottom-0 flex items-center justify-center"><a href={theHref} className="text-3xl rotate-90 md:rotate-0 hover:cursor-[url('/image/linkCursor.png'),_pointer]">{`>`}</a></div>
     )
 }
 export default Navigation;
