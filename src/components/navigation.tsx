cbdb212a1f6a53e1759d953f36c5d27a28ba97a5
abolfazl <abolfazljamshidi101010@gmail.com>
2025-09-08 17:57:33 +0330

    scroll smoth

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,29 @@
+"use client";
+
 const Navigation = ({theHref} : {theHref: string}) => {
+    const handleSmoothScroll = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        const targetId = theHref.replace('#', '');
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    };
+
     return(
-        <div className="absolute w-full h-32 bottom-0 flex items-center justify-center"><a href={theHref} className="text-3xl rotate-90 md:rotate-0 hover:cursor-[url('/image/linkCursor.png'),_pointer]">{`>`}</a></div>
+        <div className="absolute w-full h-32 bottom-0 flex items-center justify-center">
+            <a 
+                href={theHref} 
+                onClick={handleSmoothScroll}
+                className="text-3xl rotate-90 md:rotate-0 hover:cursor-[url('/image/linkCursor.png'),_pointer] transition-transform duration-300"
+            >
+                {`>`}
+            </a>
+        </div>
     )
 }
 export default Navigation;
